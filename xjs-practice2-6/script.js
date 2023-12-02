c6bfed6e3d2cc23498d6d9ffb70433e12de59fb1
Malin Håkansson <malin.hson@gmail.com>
2023-12-02 13:14:33 +0100

    Practice 2-6

---
+++ xjs-practice2-6/script.js
@@ -0,0 +1,7 @@
+/*var favouriteColour = prompt("What is your favourite colour?");
+console.log("Your favourite colour is " + favouriteColour + "!");*/
+
+var name = prompt("What is your name?");
+var year = prompt("What year were you born?");
+
+console.log(`${name} is ${2023 - year} years old.`);
