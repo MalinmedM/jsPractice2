4ddcf977a53af83e7110db1e6bf0f4ab7738b71e
Malin Håkansson <malin.hson@gmail.com>
2023-12-03 20:43:28 +0100

    Conditionals practice

---
+++ xJS-3-7-comparisons-conditionals/script.js
@@ -0,0 +1,34 @@
+/*Exercise #1
+
+var string = "12";
+var numerical = 12;
+
+if (string === numerical) {
+  console.log("Equals.");
+} else if (string == numerical) {
+  console.log("Equals!");
+} else {
+  console.log("Not equals");
+} 
+
+Exercise #2
+var sleep = Number(prompt("How many hours a night do you sleep (0-12)?"));
+
+if (sleep > 12) {
+  Number(prompt("Try again with a number between 0-12."));
+} else if (sleep >= 8) {
+  console.log("Congratulations!");
+} else if (sleep >= 5) {
+  console.log("Hm, okay but consider getting more sleep.");
+} else {
+  console.log("You really need to get more sleep!");
+}
+
+Exercise #3
+var pieces = Number(prompt("How many pieces of chocolate do you want (0-12)?"));
+var piecesLeft = 10 - pieces;
+if (piecesLeft < 5) {
+  console.log(`Only ${piecesLeft} pieces left! Stop eating!`);
+} else {
+  console.log("Have one more!");
+}*/
