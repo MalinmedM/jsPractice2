a60a21863f7b2721eb892fb25a7d6f0aba1e7b84
Malin Håkansson <malin.hson@gmail.com>
2023-12-06 19:47:07 +0100

    DOM: JS, HTML, & CSS practice

---
+++ xJS-4-practice-exercises/js/script.js
@@ -0,0 +1,23 @@
+/*var footerColour = document.querySelector("footer");
+footerColour.style.backgroundColor = "#d7ddf3";
+
+var tagline = document.querySelector("h2");
+//console.log(tagline);
+tagline.innerText = "I think the text was good as it was tbh";
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5";
+address.style.fontSize = "1.75em";
+
+var emphasize = document.querySelector("h3");
+emphasize.innerHTML = "We are here for you <em>every day</em> of the week."; */
+
+var instaGram = document.querySelector(".fa-instagram");
+instaGram.style.color = "#d45bb9";
+
+var faceBook = document.querySelector(".fa-facebook");
+faceBook.style.color = "#6a99b5";
+
+var open = document.querySelector("footer p");
+open.style.border = "1px solid #d45bb9";
+open.style.padding = "1em";
