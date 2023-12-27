2addaccb1a929ca9bb0af5c1d29afc0b51fc7b39
Malin Håkansson <malin.hson@gmail.com>
2023-12-27 15:02:07 +0100

    Function expressions

---
+++ xJS-lesson06-3-4/script.js
@@ -0,0 +1,13 @@
+/*var welcome = function (name) {
+  console.log(`Welcome ${name}!`);
+};
+
+welcome("Malin");*/
+
+var addTogether = function (num1, num2) {
+  return num1 + num2;
+};
+
+alert(addTogether(23, 5));
+console.log(addTogether(56, -233));
+var howMuch = addTogether(16, 22);
