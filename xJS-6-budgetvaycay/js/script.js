07452ed6b4c30b4cbd280e6ea27832a98d64bddf
Malin Håkansson <malin.hson@gmail.com>
2023-12-31 22:53:02 +0100

    Select +user input +calculate

---
+++ xJS-6-budgetvaycay/js/script.js
@@ -0,0 +1,11 @@
+var tripInfoButton = document.querySelector(".trip-info");
+var dailyBudget = document.querySelector(".daily-budget");
+
+var tripInfo = function () {
+  var totalBudget = Number(prompt("What is your total budget?"));
+  var accomodation = Number(prompt("How many days does your trip last?"));
+};
+
+var calculateDailyBudget = function (totalBudget, accomodation, numDays) {
+    var daily =
+  };
