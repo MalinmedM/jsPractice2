1d09ab9e89179cdea81129bd0b959ba0e48d965e
Malin Håkansson <malin.hson@gmail.com>
2024-01-03 18:51:16 +0100

    Exercise 1+2

--- xJS-06-practice/script.js
+++ xJS-06-practice/script.js
@@ -1,6 +1,19 @@
+/* Exercise 1
 var shoutIt = function (name) {
   var shout = `Hello, ${name}, nice to meet you!`.toUpperCase();
   return shout;
 };
 console.log(shoutIt("Malin"));
-console.log(shoutIt("Staffan"));
+console.log(shoutIt("Staffan"));*/
+
+/* Exercise 2
+var multiply = function (num1, num2) {
+  if (typeof num1 !== "number") {
+    return "You need to produce two numbers";
+  } else if (typeof num2 !== "number") {
+    return "You need to produce two numbers";
+  }
+  return num1 * num2;
+};
+
+console.log(multiply(8, "ek"));*/
