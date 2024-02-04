c62533e9ee84d7c7d6cb64a4e3327c31f2db3656
Malin Håkansson <malin.hson@gmail.com>
2024-02-04 20:41:12 +0100

    Created change events

--- xJS-07-challenge02/js/script.js
+++ xJS-07-challenge02/js/script.js
@@ -1,10 +1,26 @@
 // Your JS code here
 var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
 var result = document.querySelector(".result");
 var subType = "basic";
 var subDuration = 1;
 
 subTypeElement.addEventListener("change", function (e) {
+  //the target is the option element in html.
   subType = e.target.value;
   console.log(subType);
 });
+
+subDurationElement.addEventListener("change", function (e) {
+  subDuration = Number(e.target.value);
+  console.log(subDuration);
+});
+
+var updateSubscriptionDiv = function () {
+  var monthlyCost = 5;
+  if (subType === "standard") {
+    monthlyCost = 7;
+  } else if (subType === "premium") {
+    monthlyCost = 10;
+  }
+};
