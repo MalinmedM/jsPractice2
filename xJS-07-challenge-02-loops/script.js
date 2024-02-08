a5e2eb3f9423c286707b82bc1195a8b7949407d7
Malin Håkansson <malin.hson@gmail.com>
2024-02-08 20:56:57 +0100

    Ordered list loop

--- xJS-07-challenge-02-loops/script.js
+++ xJS-07-challenge-02-loops/script.js
@@ -18,3 +18,10 @@ console.log(programmingLanguages);
 for (var language of programmingLanguages) {
   console.log(`I want to learn ${language}`);
 }
+
+// use forEach for array, make all items upper case
+// log out index starting at '1' with the uppercase element
+// i.e.: 1. JAVASCRIPT
+programmingLanguages.forEach(function (item, index) {
+  console.log(`${index + 1}. ${item.toUpperCase()}`);
+});
