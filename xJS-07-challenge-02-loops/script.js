eeae90f11ffa3e590dbbc4b065f5acff5650f6a3
Malin Håkansson <malin.hson@gmail.com>
2024-02-08 20:48:55 +0100

    Output strings w for...of

---
+++ xJS-07-challenge-02-loops/script.js
@@ -0,0 +1,20 @@
+var programmingLanguages = [
+  "Scala",
+  "Java",
+  "Go",
+  "Swift",
+  "Cobol",
+  "JavaScript",
+  "Python",
+  "Ruby",
+  "Elm",
+  "Rust",
+  "C#",
+];
+
+// Log out the array
+console.log(programmingLanguages);
+
+for (var language of programmingLanguages) {
+  console.log(`I want to learn ${language}`);
+}
