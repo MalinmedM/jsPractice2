2c1e570709e7d87574cb64a6dfd568a0eb0074ec
Malin Håkansson <malin.hson@gmail.com>
2024-02-16 16:33:27 +0100

    Attempts to remove item from array and log it out

--- xJS-08practice09/js/script.js
+++ xJS-08practice09/js/script.js
@@ -10,8 +10,20 @@ var stuff = [
   "books"
 ];
 
+/* Log out each element with even-numbered indexes.
 stuff.forEach(function (item, index) {
   if (index % 2 === 0) {
   console.log(`${item}`);
   }
 });
+*/
+
+// Create function that will remove an element from an array
+var removeItem = function(array, item) {
+  var index = array.indexOf(item);
+  if (index !==) {
+    console.log();
+  } else {
+    console.log();
+  }
+};
