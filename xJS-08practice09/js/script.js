c4f6332e24ceca1fe037f426528876ab753a446e
Malin Håkansson <malin.hson@gmail.com>
2024-02-17 18:36:15 +0100

    Remove item from any array

--- xJS-08practice09/js/script.js
+++ xJS-08practice09/js/script.js
@@ -21,9 +21,15 @@ stuff.forEach(function (item, index) {
 // Create function that will remove an element from an array
 var removeItem = function(array, item) {
   var index = array.indexOf(item);
-  if (index !==) {
-    console.log();
+  if (index = -1) {
+    console.log(`No such element exists.`);
   } else {
-    console.log();
+    array.splice(index, 1);
+    console.log(`Removing ${item}`);
   }
 };
+
+removeItem(stuff, "blankets");
+connsole.log(stuff)
+removeItem(stuff, "hairspray");
+connsole.log(stuff)
