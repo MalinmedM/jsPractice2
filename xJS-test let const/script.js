ddc2b99664b6ea9ca535166c4c3ba3259e010e82
Malin Håkansson <malin.hson@gmail.com>
2024-02-19 10:21:47 +0100

    test let const

---
+++ xJS-test let const/script.js
@@ -0,0 +1,13 @@
+const numOfDrinks = 5;
+
+const drinks = function () {
+  const tea = 6 + numOfDrinks;
+  console.log(tea);
+};
+drinks();
+
+/*if (numOfDrinks >= 5) {
+  let soda = "lemon-lime";
+  console.log(soda);
+}
+console.log(soda);*/
