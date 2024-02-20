e331dcbb07befa14a10e8f3f2c6158a962d7b31c
Malin Håkansson <malin.hson@gmail.com>
2024-02-20 21:50:13 +0100

    Scope Math practice start

--- xjs-lesson02-challenge-1/script.js
+++
@@ -1,5 +0,0 @@
-var tempCelsius = 10;
-var toFarenheit = tempCelsius * 1.8 + 32;
-console.log(
-  `Outside my friends house it's ${tempCelsius} degrees Celsius, which is ${toFarenheit} degrees Farenheit.`,
-);
