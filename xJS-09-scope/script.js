a4fe2b361d8ee23f5a2dc3c30f91c70a0ca43f5d
Malin Håkansson <malin.hson@gmail.com>
2024-02-23 21:31:39 +0100

    Max +min finder

--- xJS-09-scope/script.js
+++ xJS-09-scope/script.js
@@ -14,10 +14,21 @@ for (let exp of expenses) {
 }
 
 // Write showMinMax function here
-const showMinMax = function(expenses) {
+const showMinMax = function (expenses) {
   const min = Math.min(...expenses);
   const max = Math.max(...expenses);
-  create listttitem
-}
+
+  const minListItem = document.createElement("li");
+  minListItem.innerText = `Min: $${min}`;
+
+  const maxListItem = document.createElement("li");
+  maxListItem.innerText = `Max: $${max}`;
+
+  results.append(maxListItem, minListItem);
+  results.classList.remove("hide");
+};
 
 // Write click event listener for 'button' and call the showMinMax function
+button.addEventListener("click", function () {
+  showMinMax(expenses);
+});
