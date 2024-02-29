d9f9fe5a20901f3d5674fdf817b7fb7d991e59bf
Malin Håkansson <malin.hson@gmail.com>
2024-02-29 11:25:12 +0100

    Objects hours done

--- xJS10-objects-hours-counter/js/script.js
+++ xJS10-objects-hours-counter/js/script.js
@@ -7,18 +7,37 @@ const dance = document.querySelector(".dance");
 const learning = {
   topic: "JS",
   learningGoals: [
-    "Understand programming fundamentals",
-    "Have Fun",
-    "Build JS programs"
+    "Build JS Programs", 
+    "Get a great job", 
+    "Feel confident", 
+    "Build beautiful websites", 
+    "Belong to a team",
   ],
   category: "Front End Development",
   topicImportance: "high",
   hoursThisWeek: 2,
-  weeklyHourGoal: 7
+  weeklyHourGoal: 7,
+  achievedStudyGoal: false,
+  addStudyTime: function(hours){
+    this.hoursThisWeek += hours;
+    if (this.hoursThisWeek >= this.weeklyHourGoal) {
+      this.achievedStudyGoal = true;
+      this.celebrate();
+    }
+  },
+  celebrate: function(){
+    body.classList.add("celebrate");
+    success.classList.remove("hide");
+    dance.classList.remove("hide");
+  }
 };
+
+console.log(learning);
 
 topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
 countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals.`;
 
 topicElement.classList.remove("hide");
 countElement.classList.remove("hide");
+
+learning.addStudyTime(10);
