cf106566f4adc0b3010c54cabdde87df714034a4
Malin Håkansson <malin.hson@gmail.com>
2024-03-03 21:26:08 +0100

    Factory functions and loops

--- xJS-11-starter/script.js
+++ xJS-11-starter/script.js
@@ -1,7 +1,9 @@
 const createOutfit = function(){
     const outfit = {
-        shirt: "green",
-        pants: "blue",
+        // replacing w parameters: shirt: "green",
+        shirt: shirt,
+        // replacing w parameters: pants: "blue",
+        pants: pants,
         isNew: false,
         showOff: function(){
             this.isNew = true;
@@ -12,3 +14,45 @@ const createOutfit = function(){
 };
 
 console.log(createOutfit());
+
+/*creating new objects w factory functions
+const tuesday = createOutfit();
+tuesday.shirt = "white";
+tuesday. pants = "black";
+console.log(tuesday);
+*/
+
+//or just do it this way with parameters above:
+const tuesday = createOutfit("white", "black");
+console.log(tuesday);
+
+const wednesday = createOutfit("green", "blue");
+console.log(wednesday);
+
+//Loop through keys only
+for (let key in tuesday){
+    console.log(key);
+};
+
+//Loop through values only
+for (let key in tuesday){
+    console.log(tuesday[key]);
+};
+
+//Loop through keys and values
+for (let key in tuesday){
+    console.log(key, tuesday[key]);
+}
+
+//Loop through multiple objects
+const outfitArray = [tuesday, wednesday];
+for (let outfit of outfitArray){
+    console.log(outfit);
+};
+
+//Combineing for...of and for...in loops to isolate keys and values of multibple objects
+for (let outfit of outfitArray) {
+    for (let key in outfit){
+        console.log(key, outfit[key]);
+    }
+};
